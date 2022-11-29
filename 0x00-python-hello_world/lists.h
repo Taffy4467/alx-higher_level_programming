46c43f4f68c47023e7d1237c5cb33164fd17e9a3
Tafadzwa <gandarit4467@gmail.com>
2022-11-28 18:08:13 -0800

    editing task 3

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,24 @@
+fndef LISTS_H
+
+#define LISTS_H
+
+
+
+#include <stdlib.:wq
+
+
+
+/**
+ *
+ *  * struct listint_s - singly linked list
+ *
+ *   * @n: integer
+ *
+ *    * @next: points to the next node
+ *
+ *     *
+ *
+ *      * Description: singly linked list node structure
+ *
+ *       * for Holberton project
+/
