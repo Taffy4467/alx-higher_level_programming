b65a547c01394f7df16da76351a426a8944d415e
Tafadzwa <gandarit4467@gmail.com>
2023-01-09 15:37:07 +0000

    update 11square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-'''Module for multi-level inheritance'''
-
+"""Module contains implementation of `Square` class"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    '''Inherits from Rectangle'''
-
+    """Inherits from class `Rectangle`"""
     def __init__(self, size):
-        '''Constructor'''
-        self.integer_validator('size', size)
-
-        super().__init__(size, size)
+        """Initializes the values"""
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(self.__size, self.__size)
 
-        def area(self):
-            '''Calculates the area of a square'''
-            return self.__size * self.__size
-
-        def __str__(self):
-            '''Return the square representation'''
-            msg = '[Square] {}/{}'.format(self.__size, self.__size)
-            return msg
+    def __str__(self):
+        """Prints the description of the `Rectangle`"""
+        string = "[Square] {}/{}".format(self.__size, self.__size)
+        return string
