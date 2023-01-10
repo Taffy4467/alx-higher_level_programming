97964afefb4c441e41b8c7ce27d36d0b729a9e51
tafadzwa <gandarit4467@gmail.com>
2023-01-10 10:45:16 +0000

    trial

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,6 +10,6 @@ filename (str): The name of the file to write.
 text (str): The text to write to the file.
 Returns:
 The number of characters written.
-"""
+    """
 with open(filename, "w", encoding="utf-8") as f:
 return f.write(text)
